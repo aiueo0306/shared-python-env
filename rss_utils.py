8bc84b364f88d26aa2ba317ff621b6aac82d3862
aiueo0306 <sato.sota@create-sd.co.jp>
2025-09-30 19:34:25 +0900

    Update rss_utils.py

--- rss_utils.py
+++ rss_utils.py
@@ -28,10 +28,11 @@ def generate_rss(items, output_path, base_url, gakkai_name):
         if pub_date is not None:
             ymd = pub_date.strftime('%Y%m%d')
         
-            entry.link(href=f"{base_url}#{ymd}")
+            entry.link(href=f"{base_url}")
         
             if link:
-                entry.guid(f"{link}#{ymd}", permalink=False)
+                entry.guid(f"{title}#{ymd}", permalink=False)
+                # entry.guid(f"{link}#{ymd}", permalink=False)
             else:
                 entry.guid(f"{title}#{ymd}", permalink=False)
         
